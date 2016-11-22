6ecd90c699ca766930c47afb49854a8085ba4640
blanchetsebastien <blanchet.sebastien1@gmail.com>
2016-11-22 09:29:58 +0100

    commit 1er changement

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,21 @@
+package geco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    @Test
+    public void testGenerateLoginForNomAndPrenom() throws Exception {
+        String[] t = {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"};
+        LoginService test = new LoginService(t);
+
+        LoginGenerator l = new LoginGenerator(test);
+        l.generateLoginForNomAndPrenom("ROL","Jean");
+
+        assertEquals(l.generateLoginForNomAndPrenom("Durant","Paul"),"PDUR");
+        /*assertEquals(l.generateLoginForNomAndPrenom("Roll","Jean"),"JROL1");
+        assertEquals(l.generateLoginForNomAndPrenom("Dùrant","Paul"),"PDUR");*/
+    }
+}
